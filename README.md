c95228077c9fb6f95689d5db4572de4166191d75
Zeno Zeng <zenoofzeng@gmail.com>
2015-03-30 20:56:20 +0800

    floodFill

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ Detect MER of Oranges
 - 基于图像 HSL 分量中饱和度分量作为二值化划分
 
 - 基于泛洪算法的孤点去除与填充
+
+    - http://blog.csdn.net/jia20003/article/details/8908464
--- mer-detect.js
+++ mer-detect.js
@@ -1,4 +1,61 @@
 /**
+ * 八邻域泛洪
+ *
+ * @param {Number} x - X
+ * @param {Number} y - Y
+ * @param {Number} w - Width
+ * @param {Number} h - Height
+ * @param {Function} test - Test function, test([this, top, right, bottom, left]) (each is canvasPixelIndex), should return bool
+ * @param {Function} op - Will call op(canvasPixelIndex), if test passed
+ */
+var floodFill = function(x, y, w, h, test, op) {
+
+    var queue = [{x: x, y: y}];
+
+    var mem = {};
+
+    while (queue.length > 0) {
+
+        var pixel = queue.pop();
+        x = pixel.x;
+        y = pixel.y;
+
+        var canvasPixelIndex = (y * w + x - 1) * 4;
+        var neighbors = [
+            {x: x - 1, y: y - 1},
+            {x: x - 1, y: y + 1},
+            {x: x + 1, y: y - 1},
+            {x: x + 1, y: y + 1},
+            {x: x - 1, y: y},
+            {x: x + 1, y: y},
+            {x: x, y: y + 1},
+            {x: x, y: y - 1},
+        ].filter(function(pixel) {
+            var x = pixel.x;
+            var y = pixel.y;
+            return (x > -1) && (y > -1) && (x < w) && (y < h);
+        });
+
+        var ret = test(neighbors.map(function(pixel) {
+            return (pixel.y * w + pixel.x - 1) * 4;
+        }).concat(canvasPixelIndex));
+
+        console.log(x, y, ret);
+
+        if (ret) {
+            op(canvasPixelIndex);
+            neighbors.forEach(function(pixel) {
+                var key = JSON.stringify(pixel);
+                if (!mem[key]) {
+                    queue.push(pixel);
+                    mem[key] = true;
+                }
+            });
+        }
+    }
+};
+
+/**
  * Binarization
  * 此方法利用图像的 HSL 分量实现选择区域，S > 50% 标记为目标区域
  *
@@ -17,13 +74,35 @@ var binarization = function(canvas, putImageData) {
         var r = pixels[i];
         var g = pixels[i+1];
         var b = pixels[i+2];
-        var s = (window.colorConvert.rgb2hsl(r, g, b))[1];
-        if (s > 50) {
+        var hsl = (window.colorConvert.rgb2hsl(r, g, b));
+        var s = hsl[1];
+        var l = hsl[2];
+        if (s > 30 && l > 5) {
             pixels[i+3] = 255;
         } else {
-            pixels[i+3] = 255 * 0.8;
+            // pixels[i+3] = 255 * 0.2;
+            pixels[i+3] = 0;
         }
     }
+
+    // flood fill
+    var test = function(_pixels) {
+        return _pixels.every(function(i) {
+            var alpha = pixels[i + 3];
+            return alpha === 255;
+        });
+    };
+    var op = function(index) {
+        pixels[index] = 255; // update R
+        // pixels[index + 1] = 0; // update G
+        // pixels[index + 2] = 0; // update B
+    };
+    floodFill(parseInt(canvas.width / 2),
+              parseInt(canvas.height / 2),
+              canvas.width,
+              canvas.height,
+              test,
+              op);
 
     if (putImageData) {
         ctx.putImageData(data, 0, 0);
